7ed55f11ae676f9ac43ae34f2eae868f26db959a
theLastKing711 <lastking711@protonmail.com>
2025-01-28 19:43:41 +0300

    a

--- app/Models/Media.php
+++ app/Models/Media.php
@@ -2,12 +2,11 @@
 
 namespace App\Models;
 
+use CloudinaryLabs\CloudinaryLaravel\CloudinaryEngine;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 /**
- * 
- *
  * @property int $id
  * @property string $medially_type
  * @property int $medially_id
@@ -17,6 +16,7 @@ use Illuminate\Database\Eloquent\Model;
  * @property int $size
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
+ *
  * @method static \Database\Factories\MediaFactory factory($count = null, $state = [])
  * @method static \Illuminate\Database\Eloquent\Builder|Media newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Media newQuery()
@@ -30,8 +30,10 @@ use Illuminate\Database\Eloquent\Model;
  * @method static \Illuminate\Database\Eloquent\Builder|Media whereMediallyType($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Media whereSize($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Media whereUpdatedAt($value)
+ *
  * @property-read Model|\Eloquent $medially
- * @method static  joinRelationship()
+ *
+ * @method static joinRelationship()
  * @method static Illuminate\Database\Eloquent\Builder<static> orderByLeftPowerJoins(string|array<string, \Illuminate\Contracts\Database\Query\Expression> $column)
  * @method static Illuminate\Database\Eloquent\Builder<static> orderByLeftPowerJoinsCount(string $column, string|null $order)
  * @method static Illuminate\Database\Eloquent\Builder<static> orderByPowerJoins(string|array<string, \Illuminate\Contracts\Database\Query\Expression> $column)
@@ -46,6 +48,7 @@ use Illuminate\Database\Eloquent\Model;
  * @method static Illuminate\Database\Eloquent\Builder<static> orderByPowerLeftJoinsSum(string $column, string|null $order)
  * @method static Illuminate\Database\Eloquent\Builder<static> powerJoinHas(string $relations, mixed operater, mixed value)
  * @method static Illuminate\Database\Eloquent\Builder<static> powerJoinWhereHas(string $relations, \Closure(Illuminate\Database\Query\JoinClause $join)|array $join_callback_or_array)
+ *
  * @mixin \Eloquent
  */
 class Media extends \CloudinaryLabs\CloudinaryLaravel\Model\Media
@@ -54,6 +57,18 @@ class Media extends \CloudinaryLabs\CloudinaryLaravel\Model\Media
     //to allow usage of factory for the model
     //and possibly add more features for the model in the future
     use HasFactory;
+
+    public static function fromCloudinaryUploadresponse(CloudinaryEngine $response_file): self
+    {
+
+        $media = new Media;
+        $media->file_name = $response_file->getFileName();
+        $media->file_url = $response_file->getSecurePath();
+        $media->size = $response_file->getSize();
+        $media->file_type = $response_file->getFileType();
+
+        return $media;
+    }
 
     protected $table = 'media';
 }
