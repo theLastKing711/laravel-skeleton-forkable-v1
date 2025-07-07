105d776a74211ddde5bef18fc3533f49e2e4a925
beshar jeiroudi <lastking711@protonmail.com>
2025-07-07 00:04:34 -0700

    'a'

--- tests/Feature/Admin/Abstractions/AdminTestCase.php
+++ tests/Feature/Admin/Abstractions/AdminTestCase.php
@@ -5,6 +5,8 @@ namespace Tests\Feature\Admin\Abstractions;
 use App\Models\User;
 use Database\Seeders\RolesAndPermissionsSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Collection;
+use Illuminate\Testing\TestResponse;
 use Tests\TestCase;
 
 class AdminTestCase extends TestCase
@@ -15,11 +17,11 @@ class AdminTestCase extends TestCase
 
     public User $admin;
 
-    public function setUp(): void
+    protected function setUp(): void
     {
         parent::setUp();
 
-//        parent::withHeader('Accept', 'application/json');
+        //        parent::withHeader('Accept', 'application/json');
 
         $this->seed(RolesAndPermissionsSeeder::class);
 
@@ -35,4 +37,39 @@ class AdminTestCase extends TestCase
                 ->staticAdmin()
                 ->create();
     }
+
+    public function getShowRoute(string $main_route, int $id)
+    {
+        return $main_route.'/'.$id;
+    }
+
+    /** @var Collection<string> description */
+    public function genereateQueryParameters(Collection $query_parameters, string $query_parameter_name = 'ids')
+    {
+
+        $query_parameter_name_with_brackets = $query_parameter_name.'[]';
+
+        $query_parameters = $query_parameters
+            ->reduce(
+                function ($prev, $curr, $index) use ($query_parameter_name_with_brackets) {
+                    if ($index === 0) {
+                        return $prev.'?'.$query_parameter_name_with_brackets.'='.$curr;
+                    }
+
+                    return $prev.'&'.$query_parameter_name_with_brackets.'='.$curr;
+                },
+                ''
+            );
+
+        return $query_parameters;
+
+    }
+
+    // public function post($uri, $data = [], $headers = []): TestResponse
+    // {
+    //     return
+    //         parent::withHeaders(['Accept' => 'application/json'])
+    //             ->post($uri, $data, $headers);
+
+    // }
 }
