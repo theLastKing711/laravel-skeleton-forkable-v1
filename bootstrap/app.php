ea89adce50dc70cd5532a569487cf4e2cafe354d
beshar jeiroudi <lastking711@protonmail.com>
2025-11-22 15:41:26 +0300

    'a'

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -24,6 +24,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->append(ParseStringToBoolInQueryParameter::class);
 
         $middleware->alias([
+            'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
+            'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
+            'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
             'optional_auth' => OptionalAuthSanctum::class,
         ]);
     })
